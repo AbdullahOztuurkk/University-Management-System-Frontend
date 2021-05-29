9b798d372e1577ebb8c21fc9e804a2888f580b31
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-05-30 01:40:56 +0300

    feat: User models has been added into the models folder.

---
+++ Models/Dto/UserLoginDto.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniOtomasyonUI.Models.Concrete.Dto
+{
+    public class UserLoginDto
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("pwd")]
+        public string Password { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniOtomasyonUI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+        public string Status { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+    }
+}
