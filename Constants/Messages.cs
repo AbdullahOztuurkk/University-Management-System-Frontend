285f353bd6aec92d1dad996d10652b247fe998f1
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-05-30 19:06:42 +0300

    feat: Added messages class for any constant value.

---
+++ Constants/Messages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniOtomasyonUI.Constants
+{
+    public static class Messages
+    {
+        public static string Invalid_Email = "Geçersiz bir mail girdiniz!";
+        public static string Not_Empty_Credentials = "Email ve şifre boş girilemez.";
+        public static string Invalid_Security_Code = "Güvenlik kodu hatalı !";
+    }
+}
