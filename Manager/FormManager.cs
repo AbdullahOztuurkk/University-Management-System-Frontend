57622df8eac58977bcb15e4a8a8b65a1530e4430
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-01 19:31:50 +0300

    feat: FormManager class has been created for manipute all forms including hidden ones.

---
+++ Manager/FormManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace UniOtomasyonUI.Manager
+{
+    public static class FormManager
+    {
+        /// <summary>
+        /// Open Hidden Form when another form is closed
+        /// </summary>
+        /// <param name="formName">Opened form name list</param>
+        public static void OpenHiddenForm(params string[] formName)
+        {
+            foreach (Form frm in Application.OpenForms)
+            {
+                if (frm.Visible == false)
+                {
+                    foreach(string name in formName)
+                    {
+                        if (frm.Name == name)
+                            frm.Visible = true;
+                    }
+                }
+            }
+        }
+    }
+}
