a2faefa2bad289155fa3c1d0e13072cf2309a458
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-01 19:29:42 +0300

    Feat: added exam dto for its 'get all' function.

---
+++ Models/Dto/GetExamsDto.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniOtomasyonUI.Models.Dto
+{
+    public class GetExamsDto
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+    }
+}
