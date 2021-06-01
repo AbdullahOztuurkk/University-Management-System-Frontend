790233d7d03c777d3ae4a8e1cefcb6d23ee47cd8
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-01 19:28:31 +0300

    fix: information form has been redesigned because of backend API.

--- Pages/Informations/Information_Form.Designer.cs
+++ Pages/Informations/Information_Form.Designer.cs
@@ -32,41 +32,27 @@ namespace UniOtomasyonUI.Pages.Informations
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Information_Form));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.Txt_Email = new System.Windows.Forms.Label();
+            this.Panel_Profile_Image = new System.Windows.Forms.Panel();
+            this.Lbl_Ad_Soyad = new System.Windows.Forms.Label();
+            this.PB_User_Image = new System.Windows.Forms.PictureBox();
             this.Txt_Status = new System.Windows.Forms.Label();
             this.Txt_Surname = new System.Windows.Forms.Label();
             this.Txt_Name = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.Btn_Change_Password = new System.Windows.Forms.Button();
-            this.Txt_New_Password_Again = new System.Windows.Forms.TextBox();
-            this.Txt_New_Password = new System.Windows.Forms.TextBox();
-            this.Txt_Current_Password = new System.Windows.Forms.TextBox();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label9 = new System.Windows.Forms.Label();
-            this.label10 = new System.Windows.Forms.Label();
-            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.DG_Lesson = new System.Windows.Forms.DataGridView();
             this.tableLayoutPanel1.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            this.groupBox2.SuspendLayout();
-            this.tableLayoutPanel2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.DG_Lesson)).BeginInit();
+            this.Panel_Profile_Image.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_User_Image)).BeginInit();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.75077F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 2.498477F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 48.75075F));
             this.tableLayoutPanel1.Controls.Add(this.groupBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.groupBox2, 2, 0);
             this.tableLayoutPanel1.Location = new System.Drawing.Point(9, 148);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 1;
@@ -76,31 +62,52 @@ namespace UniOtomasyonUI.Pages.Informations
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.Txt_Email);
+            this.groupBox1.Controls.Add(this.Panel_Profile_Image);
             this.groupBox1.Controls.Add(this.Txt_Status);
             this.groupBox1.Controls.Add(this.Txt_Surname);
             this.groupBox1.Controls.Add(this.Txt_Name);
-            this.groupBox1.Controls.Add(this.label7);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.groupBox1.Location = new System.Drawing.Point(20, 20);
             this.groupBox1.Margin = new System.Windows.Forms.Padding(20);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(331, 259);
+            this.groupBox1.Size = new System.Drawing.Size(723, 259);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Kullanıcı Bilgilerim";
             // 
-            // Txt_Email
+            // Panel_Profile_Image
             // 
-            this.Txt_Email.Location = new System.Drawing.Point(121, 218);
-            this.Txt_Email.Name = "Txt_Email";
-            this.Txt_Email.Size = new System.Drawing.Size(93, 23);
-            this.Txt_Email.TabIndex = 7;
-            this.Txt_Email.Text = "Email";
-            this.Txt_Email.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Panel_Profile_Image.BackColor = System.Drawing.Color.Gainsboro;
+            this.Panel_Profile_Image.Controls.Add(this.Lbl_Ad_Soyad);
+            this.Panel_Profile_Image.Controls.Add(this.PB_User_Image);
+            this.Panel_Profile_Image.Location = new System.Drawing.Point(497, 0);
+            this.Panel_Profile_Image.Name = "Panel_Profile_Image";
+            this.Panel_Profile_Image.Size = new System.Drawing.Size(220, 225);
+            this.Panel_Profile_Image.TabIndex = 7;
+            // 
+            // Lbl_Ad_Soyad
+            // 
+            this.Lbl_Ad_Soyad.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Lbl_Ad_Soyad.Location = new System.Drawing.Point(0, 193);
+            this.Lbl_Ad_Soyad.Name = "Lbl_Ad_Soyad";
+            this.Lbl_Ad_Soyad.Size = new System.Drawing.Size(220, 32);
+            this.Lbl_Ad_Soyad.TabIndex = 9;
+            this.Lbl_Ad_Soyad.Text = "Ad Soyad";
+            this.Lbl_Ad_Soyad.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // PB_User_Image
+            // 
+            this.PB_User_Image.Image = global::UniOtomasyonUI.Properties.Resources.user;
+            this.PB_User_Image.Location = new System.Drawing.Point(23, 11);
+            this.PB_User_Image.Name = "PB_User_Image";
+            this.PB_User_Image.Size = new System.Drawing.Size(175, 175);
+            this.PB_User_Image.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PB_User_Image.TabIndex = 8;
+            this.PB_User_Image.TabStop = false;
             // 
             // Txt_Status
             // 
@@ -129,15 +136,6 @@ namespace UniOtomasyonUI.Pages.Informations
             this.Txt_Name.Text = "Name";
             this.Txt_Name.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // label7
-            // 
-            this.label7.Location = new System.Drawing.Point(5, 218);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(93, 23);
-            this.label7.TabIndex = 3;
-            this.label7.Text = "Email :";
-            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
             // label6
             // 
             this.label6.Location = new System.Drawing.Point(5, 167);
@@ -165,155 +163,22 @@ namespace UniOtomasyonUI.Pages.Informations
             this.label4.Text = "Ad :";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.Btn_Change_Password);
-            this.groupBox2.Controls.Add(this.Txt_New_Password_Again);
-            this.groupBox2.Controls.Add(this.Txt_New_Password);
-            this.groupBox2.Controls.Add(this.Txt_Current_Password);
-            this.groupBox2.Controls.Add(this.label8);
-            this.groupBox2.Controls.Add(this.label9);
-            this.groupBox2.Controls.Add(this.label10);
-            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox2.Location = new System.Drawing.Point(410, 20);
-            this.groupBox2.Margin = new System.Windows.Forms.Padding(20);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(333, 259);
-            this.groupBox2.TabIndex = 1;
-            this.groupBox2.TabStop = false;
-            this.groupBox2.Text = "Şifreni Değiştir";
-            // 
-            // Btn_Change_Password
-            // 
-            this.Btn_Change_Password.BackColor = System.Drawing.Color.Red;
-            this.Btn_Change_Password.FlatAppearance.BorderColor = System.Drawing.Color.Red;
-            this.Btn_Change_Password.FlatAppearance.BorderSize = 0;
-            this.Btn_Change_Password.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.Btn_Change_Password.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.Btn_Change_Password.Location = new System.Drawing.Point(197, 218);
-            this.Btn_Change_Password.Name = "Btn_Change_Password";
-            this.Btn_Change_Password.Size = new System.Drawing.Size(105, 28);
-            this.Btn_Change_Password.TabIndex = 4;
-            this.Btn_Change_Password.Text = "Şifreni Değiştir";
-            this.Btn_Change_Password.UseVisualStyleBackColor = false;
-            this.Btn_Change_Password.Click += new System.EventHandler(this.Btn_Change_Password_Click);
-            // 
-            // Txt_New_Password_Again
-            // 
-            this.Txt_New_Password_Again.Location = new System.Drawing.Point(129, 168);
-            this.Txt_New_Password_Again.Name = "Txt_New_Password_Again";
-            this.Txt_New_Password_Again.PasswordChar = '*';
-            this.Txt_New_Password_Again.Size = new System.Drawing.Size(173, 23);
-            this.Txt_New_Password_Again.TabIndex = 3;
-            // 
-            // Txt_New_Password
-            // 
-            this.Txt_New_Password.Location = new System.Drawing.Point(129, 117);
-            this.Txt_New_Password.Name = "Txt_New_Password";
-            this.Txt_New_Password.PasswordChar = '*';
-            this.Txt_New_Password.Size = new System.Drawing.Size(173, 23);
-            this.Txt_New_Password.TabIndex = 2;
-            // 
-            // Txt_Current_Password
-            // 
-            this.Txt_Current_Password.Location = new System.Drawing.Point(129, 66);
-            this.Txt_Current_Password.Name = "Txt_Current_Password";
-            this.Txt_Current_Password.PasswordChar = '*';
-            this.Txt_Current_Password.Size = new System.Drawing.Size(173, 23);
-            this.Txt_Current_Password.TabIndex = 1;
-            // 
-            // label8
-            // 
-            this.label8.Location = new System.Drawing.Point(6, 167);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(104, 23);
-            this.label8.TabIndex = 10;
-            this.label8.Text = "Yeni Şifre (Tekrar)";
-            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // label9
-            // 
-            this.label9.Location = new System.Drawing.Point(6, 116);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(104, 23);
-            this.label9.TabIndex = 9;
-            this.label9.Text = "Yeni Şifre :";
-            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // label10
-            // 
-            this.label10.Location = new System.Drawing.Point(6, 65);
-            this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(104, 23);
-            this.label10.TabIndex = 8;
-            this.label10.Text = "Mevcut Şifre :";
-            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // tableLayoutPanel2
-            // 
-            this.tableLayoutPanel2.ColumnCount = 1;
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel2.Controls.Add(this.groupBox3, 0, 0);
-            this.tableLayoutPanel2.Location = new System.Drawing.Point(8, 453);
-            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
-            this.tableLayoutPanel2.RowCount = 1;
-            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel2.Size = new System.Drawing.Size(763, 299);
-            this.tableLayoutPanel2.TabIndex = 10;
-            // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.DG_Lesson);
-            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBox3.Location = new System.Drawing.Point(20, 20);
-            this.groupBox3.Margin = new System.Windows.Forms.Padding(20);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(723, 259);
-            this.groupBox3.TabIndex = 0;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Derslerim";
-            // 
-            // DG_Lesson
-            // 
-            this.DG_Lesson.AllowUserToAddRows = false;
-            this.DG_Lesson.AllowUserToDeleteRows = false;
-            this.DG_Lesson.AllowUserToResizeColumns = false;
-            this.DG_Lesson.AllowUserToResizeRows = false;
-            this.DG_Lesson.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.DG_Lesson.BackgroundColor = System.Drawing.Color.Gainsboro;
-            this.DG_Lesson.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.DG_Lesson.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.DG_Lesson.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.DG_Lesson.Location = new System.Drawing.Point(3, 19);
-            this.DG_Lesson.MultiSelect = false;
-            this.DG_Lesson.Name = "DG_Lesson";
-            this.DG_Lesson.ReadOnly = true;
-            this.DG_Lesson.RowTemplate.Height = 25;
-            this.DG_Lesson.Size = new System.Drawing.Size(717, 237);
-            this.DG_Lesson.TabIndex = 0;
-            // 
             // Information_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 808);
-            this.Controls.Add(this.tableLayoutPanel2);
+            this.ClientSize = new System.Drawing.Size(784, 503);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Information_Form";
             this.Text = "Kullanıcı Bilgilerim";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Information_Form_FormClosed);
             this.Load += new System.EventHandler(this.Information_Form_Load);
             this.Controls.SetChildIndex(this.tableLayoutPanel1, 0);
-            this.Controls.SetChildIndex(this.tableLayoutPanel2, 0);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.groupBox1.ResumeLayout(false);
-            this.groupBox2.ResumeLayout(false);
-            this.groupBox2.PerformLayout();
-            this.tableLayoutPanel2.ResumeLayout(false);
-            this.groupBox3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.DG_Lesson)).EndInit();
+            this.Panel_Profile_Image.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PB_User_Image)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -323,24 +188,14 @@ namespace UniOtomasyonUI.Pages.Informations
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label Txt_Email;
         private System.Windows.Forms.Label Txt_Status;
         private System.Windows.Forms.Label Txt_Surname;
         private System.Windows.Forms.Label Txt_Name;
-        private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button Btn_Change_Password;
-        private System.Windows.Forms.TextBox Txt_New_Password_Again;
-        private System.Windows.Forms.TextBox Txt_New_Password;
-        private System.Windows.Forms.TextBox Txt_Current_Password;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
-        private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.DataGridView DG_Lesson;
+        private System.Windows.Forms.Panel Panel_Profile_Image;
+        private System.Windows.Forms.Label Lbl_Ad_Soyad;
+        private System.Windows.Forms.PictureBox PB_User_Image;
     }
 }
--- Pages/Informations/Information_Form.cs
+++ Pages/Informations/Information_Form.cs
@@ -5,30 +5,50 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using UniOtomasyonUI.Constants;
+using UniOtomasyonUI.Manager;
+using UniOtomasyonUI.Models;
 using WinFormUI.Layout;
 
 namespace UniOtomasyonUI.Pages.Informations
 {
     public partial class Information_Form : MasterForm
     {
+        Models.User CurrentUser = Program.CURRENT_USER;
         public Information_Form()
         {
             InitializeComponent();
         }
 
-        private void Btn_Change_Password_Click(object sender, EventArgs e)
-        {
-            //code here
-        }
-
         private void Information_Form_Load(object sender, EventArgs e)
         {
-            /*
-            Txt_Name.Text=
-            Txt_Email.Text=
-            Txt_Status.Text=
-            Txt_Surname.Text=
-            */
+            Txt_Name.Text = CurrentUser.FirstName;
+            Txt_Surname.Text = CurrentUser.LastName;
+            Txt_Status.Text = GetStatus(CurrentUser.Status);
+            Lbl_Ad_Soyad.Text = String.Concat(CurrentUser.FirstName, " ", CurrentUser.LastName);
+            //PB_User_Image.Image = CurrentUser.Image;
+        }
+
+        /// <summary>
+        /// Get Turkish status
+        /// </summary>
+        /// <param name="status">User status</param>
+        /// <returns>Turkish status</returns>
+        private string GetStatus(string status)
+        {
+            switch (status)
+            {
+                case "ACTIVE": return "Aktif";
+                case "INACTIVE": return "İnAktif";
+                case "GRADUATE": return "Mezun";
+                case "LEFT": return "Terk";
+            }
+            return null;
+        }
+
+        private void Information_Form_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            FormManager.OpenHiddenForm(new Student_Form().Name, new Lecturer_Form().Name, new Administrator_Form().Name);
         }
     }
 }
