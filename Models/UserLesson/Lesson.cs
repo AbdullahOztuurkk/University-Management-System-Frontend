3214eedfd59b041741584e2c9257f5944442341d
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-03 17:25:48 +0300

    feat: Userlesson classes has been created for student's panel.

---
+++ Models/UserLesson/Lesson.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace UniOtomasyonUI.Models.UserLesson
+{
+    public class Lesson
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("credit")]
+        public int Credit { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ Models/UserLesson/UserLesson.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniOtomasyonUI.Models.UserLesson
+{
+
+    public class UserLesson
+    {
+        [JsonProperty("season")]
+        public string Season { get; set; }
+
+        [JsonProperty("average")]
+        public int Average { get; set; }
+
+        [JsonProperty("lesson")]
+        public Lesson Lesson { get; set; }
+
+        [JsonProperty("userId")]
+        public int UserId { get; set; }
+    }
+
+
+}
