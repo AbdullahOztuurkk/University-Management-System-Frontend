8dff14bccc375081cb78ce5e51330d8f5a5a31b3
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-04 14:21:16 +0300

    feat: created faculty class for get all faculties

---
+++ Models/Faculty/Faculty.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace UniOtomasyonUI.Models.Faculty
+{
+    public class Faculty
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("slugifyName")]
+        public string SlugifyName { get; set; }
+    }
+
+}
