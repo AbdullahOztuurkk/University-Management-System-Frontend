481692ffbf35a9d86a4769fb29996d697ee2bde8
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-04 19:20:57 +0300

    fix: removed password and added email property to user class

--- Models/User.cs
+++ Models/User.cs
@@ -9,9 +9,9 @@ namespace UniOtomasyonUI.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
         public string Role { get; set; }
         public string Status { get; set; }
-        public string PasswordHash { get; set; }
-        public string PasswordSalt { get; set; }
+
     }
 }
