e41bfe4256d339f3224931e8c5099300569f8f7f
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-05 00:40:53 +0300

    feat: added department and lesson classes for lesson management form.

---
+++ Models/Department/Department.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniOtomasyonUI.Models.Department
+{
+    public class Department
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("slugifyName")]
+        public string SlugifyName { get; set; }
+    }
+}
---
+++ Models/Lesson/Lesson.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniOtomasyonUI.Models.Lesson
+{
+    public class Lesson
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("credit")]
+        public int Credit { get; set; }
+    }
+}
