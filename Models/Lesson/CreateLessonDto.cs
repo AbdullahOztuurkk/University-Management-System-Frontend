bc277e84dfcbe180d75bd38dba21092bcf1cac59
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-06 17:31:07 +0300

    feat: Lesson dto has been created for create lesson

---
+++ Models/Lesson/CreateLessonDto.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+
+namespace UniOtomasyonUI.Models.Lesson
+{
+    public class CreateLessonDto
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("credit")]
+        public int Credit { get; set; }
+
+        [JsonProperty("code")]
+        public string Code { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+
+        [JsonProperty("grade")]
+        public int Grade { get; set; }
+    }
+}
