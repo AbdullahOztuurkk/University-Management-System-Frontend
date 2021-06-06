7cf9a11907a923049fd4ee2be438850823db988c
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-06 13:25:20 +0300

    feat: added message for add lesson exceptions.

--- Constants/Messages.cs
+++ Constants/Messages.cs
@@ -9,5 +9,8 @@ namespace UniOtomasyonUI.Constants
         public static string Invalid_Email = "Geçersiz bir mail girdiniz!";
         public static string Not_Empty_Credentials = "Email ve şifre boş girilemez.";
         public static string Invalid_Security_Code = "Güvenlik kodu hatalı !";
+        public static string Invalid_Lesson_Identity = "Ders oluştururken isim ve ders kodu girmek zorundasınız!";
+        public static string Invalid_Lesson_Values = "Ders oluştururken kredi ve sınıfı 0'dan büyük girmek zorundasınız!";
+        public static string Not_Empty_Department_Or_Faculty = "Ders oluştururken fakülte yada bölüm adını girmek zorundasınız!";
     }
 }
