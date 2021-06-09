15bb1fa7257467781681b209107f3c0b1dfee06f
Abdullah Öztürk <abdullahozturk99@gmail.com>
2021-06-09 19:33:35 +0300

    feat: added json property attribute to user class.

--- Models/User/User.cs
+++ Models/User/User.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
 
 namespace UniOtomasyonUI.Models
 {
     public class User
     {
+        [JsonProperty("id")]
         public int Id { get; set; }
+        [JsonProperty("firstName")]
         public string FirstName { get; set; }
+        [JsonProperty("lastName")]
         public string LastName { get; set; }
+        [JsonProperty("email")]
         public string Email { get; set; }
+        [JsonProperty("role")]
         public string Role { get; set; }
+        [JsonProperty("status")]
         public string Status { get; set; }
 
     }
